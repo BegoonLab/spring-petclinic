5ac236dbf3605aa69a78b13df8f237a45cea2f21
alex <alex@TeamCity>
2024-02-03 13:47:08 +0000

    TeamCity change in 'Spring Petclinic' project: runners of 'Deploy to AWS' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -137,7 +137,7 @@ object DeployToAws : BuildType({
         sshUpload {
             id = "ssh_deploy_runner"
             transportProtocol = SSHUpload.TransportProtocol.SCP
-            sourcePath = "*.jar"
+            sourcePath = "**/*.jar"
             targetUrl = "ec2-3-81-69-171.compute-1.amazonaws.com:/home/ubuntu/pet-clinic"
             authMethod = uploadedKey {
                 username = "ubuntu"
